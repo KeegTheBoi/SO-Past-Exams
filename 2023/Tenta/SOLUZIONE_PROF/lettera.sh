f37b5b2c5602e75a10a8b3afd4a2353a24bf11c7
Keegan Carlo Falcao - keegancarlo.falcao@studio.unibo.it <keegancarlo.falcao@studio.unibo.it>
2024-01-08 17:45:34 +0100

    added SO submodules

--- 2023/Tenta/SOLUZIONE_PROF/lettera.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-(( NUMERO=${RANDOM}%2 ))
-if (( $NUMERO == 0 )) ; then echo -n a 
-else echo -n  c
-fi
-
--- 2023/Turno/turno.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash                           
-for (( i=0;i<3;i++))
-do
-    ./figlio.sh $i &
-done
-wait
-echo Terminated
-exit 0
